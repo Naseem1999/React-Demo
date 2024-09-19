9638123a5f3d792da7264c2f1038d4f43d880c0a
naseem1999 <naseemmuhammad700@gmail.com>
2024-09-19 21:03:58 +0500

    simple modal app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1>MY Todos</h1>
+       <Todo title="learn react"/>
+       <Todo title="master react"/>
+       <Todo title="Explore the full react course"/>
+       
+    </div>
+  );
+}
+
+export default App;
